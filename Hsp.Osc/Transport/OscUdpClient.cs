33646c5798245f4c63763011b1b9077a2b6a2548
The Dumb <info@hemisphera.com>
2025-07-09 15:04:17 +0200

    Switch to UdpClient instead of pure sockets for client, allow specifying local port

--- Hsp.Osc/Transport/OscUdpClient.cs
+++ Hsp.Osc/Transport/OscUdpClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
 
@@ -7,48 +8,59 @@ namespace Hsp.Osc;
 public sealed class OscUdpClient : IOscClient
 {
   private static readonly MessageParser DefaultMessageParser = new();
-  private readonly Socket _socket;
+
+  private UdpClient? _client;
 
   public IPAddress Address { get; }
+
+  public int LocalPort { get; }
 
   public int Port { get; }
 
 
+  private bool _connected;
+
+
   public OscUdpClient(IPAddress address, int port)
+    : this(port, address, port)
+  {
+  }
+
+  public OscUdpClient(int localPort, IPAddress address, int port)
   {
     Address = address;
     Port = port;
-    _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+    LocalPort = localPort;
   }
 
 
   public async Task ConnectAsync()
   {
-    if (!_socket.Connected)
-      await _socket.ConnectAsync(Address, Port);
+    if (_connected)
+      await DisconnectAsync();
+
+    _client = new UdpClient(LocalPort);
+    _client.Connect(Address, Port);
+    _connected = true;
   }
 
   public async Task DisconnectAsync()
   {
-    try
-    {
-      if (_socket.Connected)
-        await _socket.DisconnectAsync(true);
-    }
-    catch (SocketException)
-    {
-      // ignore
-    }
+    if (!_connected) return;
+    _client?.Dispose();
+    _connected = false;
+    await Task.CompletedTask;
   }
 
   public async Task SendMessageAsync(Message message)
   {
+    if (_client == null) throw new Exception("Client not connected.");
     var bytes = DefaultMessageParser.Parse(message);
-    await _socket.SendAsync(bytes);
+    await _client.SendAsync(bytes);
   }
 
   public void Dispose()
   {
-    _socket.Dispose();
+    _client?.Dispose();
   }
 }
